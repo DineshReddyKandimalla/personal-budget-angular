fde116d8add898404675d1ce9c7935d64d47187f
DineshReddyKandimalla <dkandim2@uncc.edu>
2024-02-05 06:57:34 +0530

    Loading data from JSON file

--- server.js
+++ server.js
@@ -7,38 +7,8 @@ const port = 3000;
 
 app.use(cors());
 
-const budget = {
-  myBudget: [
-    {
-      title: "Eat out",
-      budget: 25,
-    },
-    {
-      title: "Rent",
-      budget: 275,
-    },
-    {
-      title: "Grocery",
-      budget: 110,
-    },
-    {
-      title: "Medical Expenses",
-      budget: 50,
-    },
-    {
-      title: "Car Insurance",
-      budget: 30,
-    },
-    {
-      title: "Shopping",
-      budget: 100,
-    },
-    {
-      title: "Health Insurance",
-      budget: 25,
-    },
-  ],
-};
+const budget = require("./budget.json");
+
 app.use("/", express.static("public"));
 app.get("/budget", (req, res) => {
   res.json(budget);
